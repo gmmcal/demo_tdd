48e1f1acaf96243e8b1f2bdea38a50e0beb62823
Gustavo Cunha <gustavocunha@gmail.com>
2015-08-08 16:58:34 -0300

    Add post spec

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+  it { expect(subject).to validate_presence_of(:name) }
+  it { expect(subject).to validate_presence_of(:description) }
+
+  it 'has a valid factory' do
+    expect(build(:post)).to be_valid
+  end
+
+  context 'is invalid' do
+    it 'without a name' do
+      post = build(:post, name: nil)
+      post.valid?
+      expect(post.errors[:name]).to include("can't be blank")
+    end
+    it 'without a description' do
+      post = build(:post, description: nil)
+      post.valid?
+      expect(post.errors[:description]).to include("can't be blank")
+    end
+  end
+
+  context 'is valid' do
+  end
+end
